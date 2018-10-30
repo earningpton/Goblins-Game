810d94f71b1ee473ab6c2b659b52df5327c191c1
earningpton <37228145+earningpton@users.noreply.github.com>
2018-10-30 02:21:17 -0400

    a

--- docs/title.js
+++ docs/title.js
@@ -1,1 +1,7 @@
 var toggle = document.getElementById('titlescreen_audio'); toggle.muted = !toggle.muted;
+
+var x = document.getElementById("titlescreen_audio");
+
+function playTitle() {
+    x.play();
+}
