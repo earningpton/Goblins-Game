36b51b47f2b1b984e12ac9d521857fa57ffa1c3a
earningpton <37228145+earningpton@users.noreply.github.com>
2018-10-30 01:40:39 -0400

    fix toggle

--- docs/title.js
+++ docs/title.js
@@ -1,4 +1,5 @@
-var toggle = document.getElementById('my_audio'); toggle.muted = !toggle.muted;
+var toggle = document.getElementById('titlescreen_audio_audio'); toggle.muted = !toggle.muted;
+
 window.onload = function() {
-    document.getElementById("my_audio").play();
+    document.getElementById("titlescreen_audio_audio").play();
 }
